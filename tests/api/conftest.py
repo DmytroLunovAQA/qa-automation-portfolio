c0d0bc8bc9095821de175a331ddf5d560f958907
Dmytro Lunov <dmitriy.lunov@gmail.com>
2025-12-21 15:41:55 +0200

    test_create_and_verify_pet created

--- tests/api/conftest.py
+++ tests/api/conftest.py
@@ -16,3 +16,29 @@ def petstore_api():
     api = APIClient(Config.PETSTORE_BASE_URL)
     yield api
     api.close()
+
+
+@pytest.fixture
+def create_test_pet(petstore_api):
+    """
+    Fixture: Creates a text pet and deletes it after the test
+    """
+    pet_data = {
+        "id": 12345,
+        "name": "Test Dog",
+        "status": "available",
+        "category": {"id": 1, "name": "Dogs"},
+        "photoUrls": ["https://example.com/photo.jpg"],
+        "tags": [{"id": 1, "name": "test"}],
+    }
+
+    response = petstore_api.post("/pet", data=pet_data)
+    created_pet = response.json() if response.status_code == 200 else pet_data
+
+    yield created_pet  # return created pet into test
+
+    # Teardown: delete pet after test
+    try:
+        petstore_api.delete(f"/pet/{created_pet['id']}")
+    except Exception as e:
+        print(f"Can't delete a pet: {e}")
--- tests/api/test_petstore_pet.py
+++ tests/api/test_petstore_pet.py
@@ -2,12 +2,14 @@ import pytest
 from config.config import Config
 
 
+@pytest.mark.api
 class TestPetEndpoints:
 
     def test_get_pet_by_id_positive(self, petstore_api):
         pet_id = 1
 
         response = petstore_api.get(f"/pet/{pet_id}")
+
         assert (
             response.status_code == 200
         ), f"Expected status code is 200, actual is {response.status_code}"
@@ -23,9 +25,23 @@ class TestPetEndpoints:
 
     def test_get_pet_by_id_negative(self, petstore_api):
         pet_id = 99999999
+
         response = petstore_api.get(f"/pet/{pet_id}")
+
         assert (
             response.status_code == 404
         ), f"Expected status code is 404, actual is {response.status_code}"
 
         print(f"Pet ID {pet_id} not found (Expected Result)")
+
+    def test_create_and_verify_pet(self, create_test_pet, petstore_api):
+        pet_id = create_test_pet["id"]
+
+        response = petstore_api.get(f"/pet/{pet_id}")
+
+        assert response.status_code == 200
+        data = response.json()
+        assert data["name"] == "Test Dog"
+        assert data["status"] == "available"
+
+        print(f"Pet was successfuly created: {data.get('name')}")
